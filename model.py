05b162156292082c64de67ab1b48d72d68c09879
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-04-17 10:45:59 -0700

    created model class for category

--- model.py
+++ model.py
@@ -15,3 +15,15 @@ class Business(object):
     business_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     address = db.Column(db.String(150))
+
+
+class Category(object):
+    """categories of the businesses in the db"""
+
+    __tablename__ = "categories"
+
+    business_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    category = db.Column(db.Integer, db.ForeignKey("businesses.business_id"),
+                         nullable=False)
+
+    businesses = db.relationship("Business", backref=db.backref("businesses"))
