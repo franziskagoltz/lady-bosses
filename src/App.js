8f2dc45c1d1c27615d25c050b8ec7c97e4fea1f3
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-04-23 20:41:12 -0700

    added navbar template to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fetch from 'isomorphic-fetch'
+
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="#">Brand</a>
+    </div>
+
+   
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+        <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        
+      </ul>
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
     );
   }
 }
