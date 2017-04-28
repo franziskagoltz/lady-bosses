06526b2d20a3ee48ff27b8df4e1f2dd48c1dbbea
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-04-27 23:30:41 -0700

    removed navbar jsx code, as Navbar is it's own component after refactoring

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import fetch from 'isomorphic-fetch'
+import Business from "./Business"
+import Navbar from "./Navbar"
+import CategorySelect from "./CategorySelect"
+// import Places from "./GoogleMaps"
 
 
 class App extends Component {
 
   render() {
+
     return (
-      <nav className="navbar navbar-default">
-  <div className="container-fluid">
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a className="navbar-brand" href="#">Brand</a>
-    </div>
-
-   
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul className="nav navbar-nav">
-        <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        
-      </ul>
-      <ul className="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        
-      </ul>
-    </div>
-  </div>
-</nav>
-
+      <div>
+        <Navbar />
+        <CategorySelect />
+        <Business />
+      </div>
     );
   }
 }
